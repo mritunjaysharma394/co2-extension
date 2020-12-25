0418e04becd5e678b6f70471ea791bd6cada7e36
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2020-12-25 23:56:46 +0530

    added listener (function to be added)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-//1
 // form fields
-// results divs
+const form = document.querySelector('.form-data');
+const region = document.querySelector('.region-name');
+const apiKey = document.querySelector('.api-key');
 
-//6
-//call the API
+// results
+const errors = document.querySelector('.errors');
+const loading = document.querySelector('.loading');
+const results = document.querySelector('.result-container');
+const usage = document.querySelector('.carbon-usage');
+const fossilfuel = document.querySelector('.fossil-fuel');
+const myregion = document.querySelector('.my-region');
+const clearBtn = document.querySelector('.clear-btn');
 
-//5
-//set up user's api key and region
-
-//4
-// handle form submission
-
-//3 initial checks
-
-//2
-// set listeners and start app
+form.addEventListener('submit', (e) => handleSubmit(e));
+clearBtn.addEventListener('click', (e) => reset(e));
+init();
